779195c2e9d0ba085382e14ffa321e9c74e4e724
tomgar7 <56299613+tomgar7@users.noreply.github.com>
2019-10-21 23:27:41 +0200

    upload my versions

---
+++ Cwiczenia3/c03z02v2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int n1, n2, tab1[10] = {0}, tab2[10] = {0};
+bool check = true;
+
+int main() {
+    cin >> n1 >> n2;
+
+    while(n1 > 0) {
+        tab1[n1%10]++;
+        n1 /= 10;
+    }
+
+    while(n2 > 0) {
+        tab2[n2%10]++;
+        n2 /= 10;
+    }
+
+    for(int i = 0; i < 10; i++) {
+        if(tab1[i] != tab2[i]) {
+            check = false;
+            break;
+        }
+    }
+
+    if(check) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+    return 0;
+}
---
+++ Cwiczenia3/c03z03v2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int MAX = 2000000000;
+int n, i;
+bool tab[MAX];
+
+int main() {
+    cin >> n;
+
+    for(int j = 2; j <=n; j++) tab[j] = true;
+
+    for(i = 2; i < sqrt(n); i++) {
+        while(!tab[i]) i++;
+        for(int k = 2*i; k <= n; k+=i) tab[k] = false;
+    }
+
+    for(int j = 2; j <= n; j++) {
+        if(tab[j]) cout << j << " ";
+    }
+
+    return 0;
+}
