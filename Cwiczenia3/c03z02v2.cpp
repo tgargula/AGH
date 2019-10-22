707147ef64052daa220edc681ccd3fc498089936
tomgar7 <tomaszgargula1@gmail.com>
2019-10-22 12:27:34 +0200

    add new file

--- Cwiczenia3/c03z02v2.cpp
+++ Cwiczenia3/c03z02v2.cpp
@@ -8,20 +8,22 @@ bool check = true;
 int main() {
     cin >> n1 >> n2;
 
-    while(n1 > 0) {
-        tab1[n1%10]++;
-        n1 /= 10;
-    }
+    if(n1 != n2) {
+        while(n1 > 0) {
+            tab1[n1%10]++;
+            n1 /= 10;
+        }
 
-    while(n2 > 0) {
-        tab2[n2%10]++;
-        n2 /= 10;
-    }
+        while(n2 > 0) {
+            tab2[n2%10]++;
+            n2 /= 10;
+        }
 
-    for(int i = 0; i < 10; i++) {
-        if(tab1[i] != tab2[i]) {
-            check = false;
-            break;
+        for(int i = 0; i < 10; i++) {
+            if(tab1[i] != tab2[i]) {
+                check = false;
+                break;
+            }
         }
     }
 
