e2d21688e9177fac8315190342d6f7479da6acda
msluszniak <56299341+msluszniak@users.noreply.github.com>
2019-11-06 11:59:16 +0100

    add c04z01m.cpp

--- WDI/Cwiczenia4/c04z01m.cpp
+++ WDI/Cwiczenia4/c04z01m.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <algorithm>
 #include <cmath>
@@ -7,41 +5,46 @@
 
 using namespace std;
 
-const int MAX = 31;
-double tab[MAX][MAX];
+const int MAX=4;
 
-int main(){
-    int t=MAX/2;
-    int k=MAX/2;
+void Spir(int tab[MAX][MAX]){
+    int t=MAX/2 , k=MAX/2;
     tab[k][t]=1;
     int temp=1;
-    int ks, ts;
-
-        for(int i=1;temp<=(MAX)*(MAX);i++){
-            ks=i;
-            ts=i;
-            do{
-            if(temp==MAX*MAX) goto koniec;
-            temp++;
-            ts--;
-            t=t+pow(-1,i);
-            tab[k][t]=temp;
-            }
-            while(ts>0);
-
-            do{
-            if(temp==MAX*MAX) goto koniec;
-            temp++;
-            ks--;
-            k=k+pow(-1,i);
-            tab[k][t]=temp;}
-            while(ks>0);
+    int ks,ts;
+    bool flag;
+    for(int i=1;temp<=(MAX)*(MAX) && flag;i++){
+        ks=i;
+        ts=i;
+        do{
+        if(temp==MAX*MAX){
+        flag=false;
+        break; }
+        temp++;
+        ts--;
+        t=t+pow(-1,i);
+        tab[k][t]=temp;
         }
+        while(ts>0);
 
-    koniec:
-        for(int i=0;i<=MAX-1;i++){
+        do{
+        if(temp==MAX*MAX){
+        flag=false;
+        break; }
+        temp++;
+        ks--;
+        k=k+pow(-1,i);
+        tab[k][t]=temp;}
+        while(ks>0);
+    }
+}
+int main(){
+    int tab[MAX][MAX];
+    Spir(tab);
+    for(int i=0;i<=MAX-1;i++){
         for(int j=0;j<=MAX-1;j++){
         cout<<tab[j][i]<< "\t";}
         cout<<endl;
-        }
+    }
 }
+
