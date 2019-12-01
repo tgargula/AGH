7f0d03b7ad46740b19058dbdc05c29ecc000c86b
tomgar7 <56299613+tomgar7@users.noreply.github.com>
2019-12-01 19:49:29 +0100

    Update c07z17t.cpp

--- WDI/Cwiczenia7/c07z17t.cpp
+++ WDI/Cwiczenia7/c07z17t.cpp
@@ -23,11 +23,12 @@ void output(int t[N][N]) {
 
 bool marchOfKing (int t[N][N], int w, int k, int last) { //marchOfKing(t,w,k,INT32_MAX)
     if (k == N or w == N) return false;
-    if (t[w][k] >= last) return false;
+    int temp = t[w][k]; while(temp >= 10) temp /= 10;  
+    if (temp >= last) return false;
     if (w == N-1 and k == N-1) return true;
-    return marchOfKing(t, w+1, k, t[w][k]) or
-           marchOfKing(t, w+1, k+1, t[w][k]) or
-           marchOfKing(t, w, k+1, t[w][k]);
+    return marchOfKing(t, w+1, k, t[w][k]%10) or
+           marchOfKing(t, w+1, k+1, t[w][k]%10) or
+           marchOfKing(t, w, k+1, t[w][k]%10);
 }
 
 int main() {
