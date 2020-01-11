e1b11a1c7e2b5c569fe10c2c91050a7c7eaa98d9
Szymon Rys <szymon.rys@awesome-ind.com>
2020-01-11 22:24:26 +0100

    Kol1 tasks

--- WDI/functionsTest.cpp
+++ WDI/functionsTest.cpp
@@ -6,18 +6,21 @@ using namespace std;
 
 
 int main() {
-    singleNodePointer *first = createList();
+    int t[5];
+    RandomIntArray(t, 10);
+    PrintIntArray(t);
+    // singleNodePointer *first = createList();
     // singleNodePointer *first = createCycleList(100);
     // printListElements(first);
     // insertLast(first, 999);
     // testInsertLast(first, 111);
-    printListElements(first);
+    // printListElements(first);
     // deleteLast(first);
     // deleteByValue(first, 5);
     // insertByOrder(first, 1, 1555);
     // deleteByOrder(first, 1);
-    first = listRverse(first);
-    printListElements(first);
+    // first = listRverse(first);
+    // printListElements(first);
     // connectLastElementToFirst(first);
     // printListElements(first, true);
 
