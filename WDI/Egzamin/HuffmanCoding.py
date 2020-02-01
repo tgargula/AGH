0a26e3ffe9006e0f58c07f7e629a391060f9d93b
Szymon Rys <szymon.rys@awesome-ind.com>
2020-02-01 21:15:01 +0100

    small names imporve

--- WDI/Egzamin/HuffmanCoding.py
+++ WDI/Egzamin/HuffmanCoding.py
@@ -1,30 +1,31 @@
 # Programme translates inputted string into Huffman Code
 
 import operator
+import json
 
 string = input('Input a string: ')
 
-table = {}
+dictionary = {}
 
 for key in range (len(string)) :
-    if string[key] in table :
-        table[string[key]] += 1
+    if string[key] in dictionary :
+        dictionary[string[key]] += 1
     else :
-        table[string[key]] = 1
+        dictionary[string[key]] = 1
 
-table = dict(sorted(table.items(), key=operator.itemgetter(1), reverse=True))
+dictionary = dict(sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True))
 
 i = 0
-for key in table :
-    if i+1 == len(table) :
-        table[key] = i*'1'
+for key in dictionary :
+    if i+1 == len(dictionary) :
+        dictionary[key] = i*'1'
     else :
-        table[key] = i*'1' + '0'
+        dictionary[key] = i*'1' + '0'
     i += 1
 
-print (table)
+print (json.dumps(dictionary, indent=4))
 
 for i in range(len(string)) :
-    print (table[string[i]], end="")
+    print (dictionary[string[i]], end="")
 
 print()
--- WDI/Egzamin/basicCoding.py
+++ WDI/Egzamin/basicCoding.py
@@ -1,24 +1,26 @@
 # Programme translates inputted code into basic coding
 
+import json
+
 string = input('Input a code: ')
 
-table = {}
+dictionary = {}
 
 for key in range (len(string)) :
-    table[string[key]] = 1
+    dictionary[string[key]] = 1
 
 codeLength = 0
-while 2**codeLength < len(table) :
+while 2**codeLength < len(dictionary) :
     codeLength += 1
 
 i = 0
-for key in table.keys():
-    table[key] = ('0')*(codeLength-len(bin(i))+2) + bin(i)[2:]
+for key in dictionary.keys():
+    dictionary[key] = ('0')*(codeLength-len(bin(i))+2) + bin(i)[2:]
     i += 1
 
-print (table)
+print (json.dumps(dictionary, indent=4))
 
 for i in range(len(string)) :
-    print (table[string[i]], end="")
+    print (dictionary[string[i]], end="")
 
 print()
