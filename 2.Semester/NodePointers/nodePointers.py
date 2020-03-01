d408062e35b39b00ad1a3811deef13af3adbc803
tomgar7 <tomaszgargula1@gmail.com>
2020-03-01 15:31:38 +0100

    Update nodePointers.py

--- 2.Semester/NodePointers/nodePointers.py
+++ 2.Semester/NodePointers/nodePointers.py
@@ -1,42 +1,29 @@
-# It has to be continued
-
-
 class Node:
     def __init__(self):
         self.next = None
         self.value = None
 
 
-    def printNode (L):
-        while L.next != None:
-            print (L.next.value, end=" ")
-            L.next = L.next.next
-        print()
+def printNode (L):
+    while L.next != None:
+        print (L.next.value, end=" ")
+        L = L.next
+    print()
 
 
-    def insert (L, value):
-        temp = Node()
-        temp.value = value
-        while L.next != None : #and L.next.value < value:
-            L.next = L.next.next
+def insert (L, value):
+    temp = Node()
+    temp.value = value
+    while L.next != None and L.next.value < value:
+        L = L.next
 
-        temp.next = L.next
-        L.next = temp
-
-
-    def reverse (L):
-        if L.next == None: return L
-        T = L.next
-        H = reverse(T)
-        T.next = L
-        H.next = None
-        return H
-
+    temp.next = L.next
+    L.next = temp
 
 
 L = Node()
 
 for i in range(10):
-    Node.insert(L,i)
+    insert(L,i%3)
 
-Node.printNode(L)
+printNode(L)
