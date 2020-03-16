2a1fc270c836ebbfb12b1c449fdc9a2fbaa9af64
Tomek <tgargula@student.agh.edu.pl>
2020-03-16 14:32:02 +0100

    Update domki.c (100%)

--- Sem2/PI/Dekster/LAB-03/domki.c
+++ Sem2/PI/Dekster/LAB-03/domki.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int findIndex (int *tab, int N, long long sum) {
+    
+    if (N < 2) return 0;
+    
+    if (N == 2) {
+    
+        if (tab[0] == 0) return 1;
+        if (tab[1] == 0) return 0;
+    
+    }
+
+    long long sum1 = tab[0];
+    int temp = tab[1];
+    long long sum2 = sum - sum1 - temp;
+    int index = 1;
+
+    while (sum1 != sum2) {
+        
+        sum1 += temp;
+        temp = tab[++index];
+        sum2 -= temp;
+    
+    }
+
+    return index;
+
+}
+
 int main (void) {
 
     int N;
     scanf("%d", &N);
 
-    if (N < 2) { printf("0"); return 0; }
-
     int *tab = malloc (N * sizeof(int));
-    long long sum2 = 0;
+    long long sum = 0;
     
     for (int i = 0; i < N; i++) {
+    
         scanf("%d", &tab[i]);
-        sum2 += tab[i];
+    
+        sum += tab[i];
+    
     }
 
-    if (N == 2) {
-        if (tab[0] == 0) { printf("1"); return 0; }
-        if (tab[1] == 0) { printf("0"); return 0; }
-    }
-
-    sum2 -= tab[0] + tab[1];
-    long long sum1 = tab[0];
-    int temp = tab[1];
-    int index = 1;
-
-    while (sum1 != sum2) {
-        sum1 += temp;
-        temp = tab[++index];
-        sum2 -= temp;
-    }
+    int index = findIndex(tab, N, sum);
 
     printf("%d\n", index);
 
