72e6f3a45549733d0febd4b99fb138ad920342c7
tomgar7 <tomgar7@github.com>
2020-04-19 20:08:28 +0200

    Update sortowanie_slow.py

--- Sem2/ASD/Tasks/1-SortowaniaLiniowe/sortowanie_slow.py
+++ Sem2/ASD/Tasks/1-SortowaniaLiniowe/sortowanie_slow.py
@@ -8,81 +8,70 @@
 #   Niech d będzie długością najdłuższego słowa, a n – liczbą stringów
 #       w tablicy.
 #
-#   1) Dekrementujemy i od d-1 do 0 włącznie
-#   2) Tworzymy tablicę dwóch tablic: w jednej pierwszej będziemy zapisywać
-#      elementy, których długość jest mniejsza bądź równa i,
-#      a w drugiej – resztę. Złożoność czasowa operacji: O(n) ( ≤ O(L) )
-#   3) Sortujemy drugą tablicę stabilnym counting sortem według i-tej litery
+#   1) Dekrementujemy k od d-1 do 0 włącznie
+#   2) Tworzymy nową tablicę w której zapisujemy elementy od lewej, jeśli 
+#      będziemy sortować ten wyraz oraz od prawej, jeśli nie będziemy go sortować
+#      Złożoność czasowa operacji: O(n) ( ≤ O(L) )
+#   3) Sortujemy część tablicy "do posortowania" stabilnym counting sortem według i-tej litery
 #      (czyli zmodyfikowany radix_sort do stringów)
-#      Złożoność czasowa operacji: O(len(split_tab[1]) dla każdego i)
+#      Złożoność czasowa operacji: O(len(*część_do_posortowania*) dla każdego k)
 #      – łącznie O(L)
 #   4) Konkatenujemy obie tablice i nadpisujemy tablicę wejściową
 #
 # Uwagi:
 #   1) ord() – funkcja ord() zwraca liczbę przypisaną do znaku w ASCII
 #      (Niestety nie ma tak fajnie w tym przypadku jak w C/C++)
-#   2) Zrobiłem split_tab w formie tablicy dwóch tablic, bo wydaje mi się
-#      to czytelniejsze. Można również zrobić zwykłą tablicę, wpisywać
-#      wartości od początku, jeśli nie sortujemy oraz od końca – jeśli
-#      sortujemy. Potem sortować split_tab[k:], gdzie k to liczba
-#      elementów, których nie sortujemy – ten sposób jest o tyle lepszy,
-#      że nie trzeba konkatenować tablic.
-#   3) Korzystam z tego, że może definiować funkcje wewnątrz funkcji
+#   2) Korzystam z tego, że może definiować funkcje wewnątrz funkcji
 #      – oczywiście tak nie trzeba robić (ale osobiście wydaje mi się to
 #      nieco czytelniejsze)
-#   4) reversed() – funkcja po prostu odwracająca kolejność.
+#   3) reversed() – funkcja po prostu odwracająca kolejność.
 #      Przykład:
 #      for i in range(n-1, -1, -1) to to samo co: for i in reversed(range(n)),
 #      ale z reversed wygląda imho ładniej
 
+how_many_letters = ord('z') - ord('a') + 1
 
-def sort_strings (tab):
+def sort_strings (A):
 
-    def counting_sort (tab, i):
-        n = len(tab)
-        sorted_tab = [None] * n
-        how_many_letters = ord('z') - ord('a') + 1
-        temp_tab = [0] * how_many_letters
+    def counting_sort (B, i):
+        n = len(B)
+        sorted_strings = [None] * n
+        temp = [0] * how_many_letters
 
-        for string in tab: temp_tab [ ord(string[i]) - ord('a') ] += 1
+        for string in B: temp [ ord(string[i]) - ord('a') ] += 1
 
-        for j in range(1, how_many_letters): temp_tab[j] += temp_tab[j-1]
+        for j in range(1, how_many_letters): temp[j] += temp[j-1]
 
-        for string in reversed(tab):
-            temp_tab [ ord(string[i]) - ord('a') ] -= 1
-            sorted_tab [ temp_tab [ ord(string[i]) - ord('a') ] ] = string
-
-        return sorted_tab
-
-    
-    def split_tab (tab, i):
-        split_tab = [ [], [] ]
-        for string in tab:
-            if len(string) <= i: split_tab[0].append(string)
-            else:                split_tab[1].append(string)
+        for string in B:
+            temp [ ord(string[i]) - ord('a') ] -= 1
+            sorted_strings [ temp [ ord(string[i]) - ord('a') ] ] = string
         
-        return split_tab
-    
-
-    def concatenate_tab (tab1, tab2):
-        tab = tab1
-        tab.extend(tab2)
-        return tab
+        return sorted_strings
 
 
-    d = len(max(tab, key=len))
-    n = len(tab)
+    d = len(max(A, key=len))
+    n = len(A)
 
-    for i in reversed(range(d)):
-        split_list = split_tab(tab, i)
-        split_list[1] = counting_sort(split_list[1], i)
-        tab = concatenate_tab(split_list[0], split_list[1])
-    
-    return tab
+    for k in reversed(range(d)):
+        temp = [None] * n
+        i = 0; j = n
+        for string in A:
+            if len(string) > k:
+                temp[i] = string
+                i += 1
+            else:
+                j -= 1
+                temp[j] = string
+
+        sorted_strings = counting_sort(temp[:i], k)
+        sorted_strings.extend(temp[i:])
+        A = sorted_strings[:]
+
+    return A
 
 
 
-tab = [
+A = [
     "prosze",
     "zaproponowac",
     "algorytm",
@@ -95,11 +84,11 @@ tab = [
     "slow"
 ]
     
-print(tab)
+print(A)
 
-tab = sort_strings(tab)
+A = sort_strings(A)
 
-print(tab)
+print(A)
 
 
 L = [
