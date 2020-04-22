0c491f6a576203735920952353fcb9d5647a1720
tomgar7 <tomgar7@github.com>
2020-04-22 21:52:35 +0200

    Update sortowanie_slow.py so that it is actually O(L)

--- Sem2/ASD/Tasks/1-SortowaniaLiniowe/sortowanie_slow.py
+++ Sem2/ASD/Tasks/1-SortowaniaLiniowe/sortowanie_slow.py
@@ -9,67 +9,54 @@
 #       w tablicy.
 #
 #   1) Dekrementujemy k od d-1 do 0 włącznie
-#   2) Tworzymy nową tablicę w której zapisujemy elementy od lewej, jeśli 
-#      będziemy sortować ten wyraz oraz od prawej, jeśli nie będziemy go sortować
-#      Złożoność czasowa operacji: O(n) ( ≤ O(L) )
-#   3) Sortujemy część tablicy "do posortowania" stabilnym counting sortem według i-tej litery
-#      (czyli zmodyfikowany radix_sort do stringów)
-#      Złożoność czasowa operacji: O(len(*część_do_posortowania*) dla każdego k)
-#      – łącznie O(L)
-#   4) Konkatenujemy obie tablice i nadpisujemy tablicę wejściową
+#   2) Sortujemy tablicę stabilnym counting sortem po długości O(n)
+#   3) Definiujemy iterator i, który wyznacza miejsce w tablicy, od którego będziemy sortować
+#      (Stworzenie O(1), iteracja O(n))
+#   4) Sortujemy część tablicę od i-tego miejsca do końca stabilnym counting sortem
+#      według k-tej litery (czyli zmodyfikowany radix_sort do stringów)
+#      Złożoność czasowa operacji: O(len(*część_do_posortowania*) dla każdego k) – łącznie O(L)
+#   5) Nadpisujemy posortowaną część
 #
 # Uwagi:
 #   1) ord() – funkcja ord() zwraca liczbę przypisaną do znaku w ASCII
 #      (Niestety nie ma tak fajnie w tym przypadku jak w C/C++)
-#   2) Korzystam z tego, że może definiować funkcje wewnątrz funkcji
-#      – oczywiście tak nie trzeba robić (ale osobiście wydaje mi się to
-#      nieco czytelniejsze)
-#   3) reversed() – funkcja po prostu odwracająca kolejność.
+#   2) reversed() – funkcja po prostu odwracająca kolejność.
 #      Przykład:
 #      for i in range(n-1, -1, -1) to to samo co: for i in reversed(range(n)),
 #      ale z reversed wygląda imho ładniej
 
-how_many_letters = ord('z') - ord('a') + 1
+LETTERS = ord('z') - ord('a') + 1
+
+def counting_sort_by_length(A, d):
+    n = len(A)
+    B = [None] * n
+    C = [0] * (d+1)
+    for i in range(n): C[len(A[i])] += 1
+    for i in range(1, d+1): C[i] += C[i-1]
+    for i in range(n):
+        C[len(A[i])] -= 1
+        B[C[len(A[i])]] = A[i]
+    A[:] = B
+
+def counting_sort_by_letter(D, i):
+    n = len(D)
+    sorted_strings = [None] * n
+    C = [0] * LETTERS
+    for j in range(n): C [ ord(D[j][i]) - ord('a') ] += 1
+    for j in range(1, LETTERS): C[j] += C[j-1]
+    for j in reversed(range(n)):
+        C [ ord(D[j][i]) - ord('a') ] -= 1
+        sorted_strings [ C [ ord(D[j][i]) - ord('a') ] ] = D[j]
+    return sorted_strings
 
 def sort_strings (A):
-
-    def counting_sort (B, i):
-        n = len(B)
-        sorted_strings = [None] * n
-        temp = [0] * how_many_letters
-
-        for string in B: temp [ ord(string[i]) - ord('a') ] += 1
-
-        for j in range(1, how_many_letters): temp[j] += temp[j-1]
-
-        for string in B:
-            temp [ ord(string[i]) - ord('a') ] -= 1
-            sorted_strings [ temp [ ord(string[i]) - ord('a') ] ] = string
-        
-        return sorted_strings
-
-
     d = len(max(A, key=len))
     n = len(A)
-
+    counting_sort_by_length(A, d)
+    i = n
     for k in reversed(range(d)):
-        temp = [None] * n
-        i = 0; j = n
-        for string in A:
-            if len(string) > k:
-                temp[i] = string
-                i += 1
-            else:
-                j -= 1
-                temp[j] = string
-
-        sorted_strings = counting_sort(temp[:i], k)
-        sorted_strings.extend(temp[i:])
-        A = sorted_strings[:]
-
-    return A
-
-
+        while i > 0 and len(A[i-1]) > k: i -= 1
+        A[i:] = counting_sort_by_letter(A[i:], k)
 
 A = [
     "prosze",
@@ -85,11 +72,8 @@ A = [
 ]
     
 print(A)
-
-A = sort_strings(A)
-
+sort_strings(A)
 print(A)
-
 
 L = [
     'abc',
@@ -99,6 +83,5 @@ L = [
     'abcdefghijkl'
 ]
 
-L = sort_strings(L)
-
+sort_strings(L)
 print(L)
